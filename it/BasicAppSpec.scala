3dedb96977782c578c6e10207e85f6bdb1ae7429
Mark Kelly <mkelly28@tcd.ie>
2017-06-05 13:51:59 +0100

    assert result back from create test

--- it/BasicAppSpec.scala
+++ it/BasicAppSpec.scala
@@ -1,10 +1,11 @@
+import java.util.UUID
+
 import api.service.models.Info
 import com.typesafe.config.{Config, ConfigFactory}
 import play.api.libs.json.{JsObject, Json}
 import services.slickbacked.InfoSpec
 
-import scalaj.http.Http
-
+import scalaj.http.{Http, HttpResponse}
 import api.service.models.JsonOps._
 
 class BasicAppSpec extends InfoSpec {
@@ -27,10 +28,16 @@ class BasicAppSpec extends InfoSpec {
 
     eventually {
       val infoToCreate = Json.toJson(new Info(name = "foo", data = dataJson.as[JsObject], meta = Seq("foo"))).toString()
-      val output = Http(s"http://$baseUrl:9000/api/sales-channels/$baseSalesChannelId/infos")
+      val output: HttpResponse[String] = Http(s"http://$baseUrl:9000/api/sales-channels/$baseSalesChannelId/infos")
         .postData(infoToCreate).asString
 
       output.code shouldBe 201
+      val result = (Json.parse(output.body) \ "id").as[String]
+      println(s"Info created Id - $result")
+
+      //will throw an exeception if not a UUID
+      val id = UUID.fromString(result)
+      id shouldBe a [UUID]
     }
   }
 
