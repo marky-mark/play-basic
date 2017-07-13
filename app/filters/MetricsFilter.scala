b37a2f8e9ce2ac4fc7e7897747c79b5988ef8387
mark kelly <mark.kelly@pb.com>
2017-07-13 17:41:00 +0100

    added metrics filter to capture everything

--- app/filters/MetricsFilter.scala
+++ app/filters/MetricsFilter.scala
@@ -8,7 +8,7 @@ import play.api.Logger
 import play.api.mvc.{Filter, RequestHeader, Result}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 object Metrics {
 }
@@ -24,17 +24,42 @@ class MetricsFilter(metricsService: MetricsService)(implicit ec: ExecutionContex
     val method = requestHeader.tags.get(play.api.routing.Router.Tags.RouteActionMethod)
     val controller = requestHeader.tags.get(play.api.routing.Router.Tags.RouteController)
 
+    metricsService.increment("active.requests")
+
+    def logCompleted(name: String, startTime: DateTime) = {
+      metricsService.decrement("active.requests")
+      metricsService.increment(name)
+      metricsService.markMeter(name)
+      metricsService.measureLatency(name, startTime)
+    }
+
     val startTime = DateTime.now()
     if (method.isEmpty || controller.isEmpty) {
       Logger.error(s"Error: cannot get method or controller name while filtering metrics. Method_name: $method, controller_name: $controller")
       nextFilter(requestHeader)
     } else {
       nextFilter(requestHeader).andThen {
-        case Success(response) if response.header.status / 100 == 2 =>
-          val name = s"api.response.2xx.${requestHeader.method}.${controller.get}.${method.get}"
-          metricsService.measureLatency(name, startTime)
-        case _ =>
+        case Success(response) =>
+          val httpCode = response.header.status / 100
+          val name = s"api.response.${httpCode}xx.${requestHeader.method}.${controller.get}.${method.get}"
+          logCompleted(name, startTime)
+        case Failure(e) =>
+          Logger.error(s"Error: Failure with exception $e")
+          logCompleted("api.response.exception", startTime)
       }
     }
+
+//    val startTime = DateTime.now()
+//    if (method.isEmpty || controller.isEmpty) {
+//      Logger.error(s"Error: cannot get method or controller name while filtering metrics. Method_name: $method, controller_name: $controller")
+//      nextFilter(requestHeader)
+//    } else {
+//      nextFilter(requestHeader).andThen {
+//        case Success(response) if response.header.status / 100 == 2 =>
+//          val name = s"api.response.2xx.${requestHeader.method}.${controller.get}.${method.get}"
+//          metricsService.measureLatency(name, startTime)
+//        case _ =>
+//      }
+//    }
   }
 }
