e1badfaee7e44f60d681d1ecfb8d526b3d08c759
mark kelly <mark.kelly@pb.com>
2017-08-14 22:23:09 +0100

    client update

--- play-basic-client/src/main/scala/com/markland/api/client/play_basic/Client.scala
+++ play-basic-client/src/main/scala/com/markland/api/client/play_basic/Client.scala
@@ -21,16 +21,32 @@ trait SalesChannelsInfosClient {
   def get(salesChannelId: tags.ids.SalesChannelId, infoId: tags.ids.InfoId, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Info]]]
   def put(salesChannelId: tags.ids.SalesChannelId, infoId: tags.ids.InfoId, body: Info, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]]
 }
-     
+
+trait InfosClient {
+
+  def listRaw(rawRequest: WSRequest, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationInfoPageJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[InfoPage]]]
+  def list(limit: Option[Int] = None, nextCursor: Option[String] = None, salesChannelId: Option[tags.ids.SalesChannelId] = None, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationInfoPageJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[InfoPage]]]
+}
+
+trait BatchSalesChannelsInfosClient {
+
+  def postRaw(rawRequest: WSRequest, body: BatchInfo, xFlowId: Option[tags.ids.FlowId], xRequestGroupId: Option[tags.ids.RequestGroupId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Seq[Problem]], Result.Success[UpdateInfos]]]
+  def post(salesChannelId: tags.ids.SalesChannelId, body: BatchInfo, xFlowId: Option[tags.ids.FlowId], xRequestGroupId: Option[tags.ids.RequestGroupId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Seq[Problem]], Result.Success[UpdateInfos]]]
+}
+
 
 trait Client {
   def salesChannelsInfos: SalesChannelsInfosClient
+  def infos: InfosClient
+  def batchSalesChannelsInfos: BatchSalesChannelsInfosClient
 }
 
 class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
   import JsonOps._
 
   override val salesChannelsInfos: SalesChannelsInfosClient = SalesChannelsInfosClient
+  override val infos: InfosClient = InfosClient
+  override val batchSalesChannelsInfos: BatchSalesChannelsInfosClient = BatchSalesChannelsInfosClient
 
   object SalesChannelsInfosClient extends SalesChannelsInfosClient {
     def listRaw(rawRequest: WSRequest, xFlowId: Option[tags.ids.FlowId], acceptEncoding: Option[String], ifModifiedSince: Option[String], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Seq[Info]]]] = {
@@ -62,7 +78,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
           Left(Result.Error(resp.status, message = Some("Unexpected response code"), rawResponse = resp))
       }
     }
-           
+
     def postRaw(rawRequest: WSRequest, body: Info, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]] = {
       require(Seq(ContentTypes.ApplicationJson) contains contentType, "Unacceptable contentType specified")
       val headers = Seq(
@@ -71,7 +87,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
         xFlowId.map("X-Flow-Id" -> _.toString)
       ).flatten
       val request = rawRequest.withHeaders(headers:_*).withRequestTimeout(requestTimeout.duration.toMillis)
-    
+
       RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]](request, "POST", _.withBody(Json.toJson(body))) {
         case resp if resp.status == 201 =>
           Right(Result.Success(resp.status, message = Some("Created"), rawResponse = resp))
@@ -85,7 +101,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
           Left(Result.Error(resp.status, message = Some("Unexpected response code"), rawResponse = resp))
       }
     }
-           
+
     def getRaw(rawRequest: WSRequest, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Info]]] = {
       require(Seq(ContentTypes.ApplicationJson, ContentTypes.ApplicationProblemJson) contains contentType, "Unacceptable contentType specified")
       val headers = Seq(
@@ -94,7 +110,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
         xFlowId.map("X-Flow-Id" -> _.toString)
       ).flatten
       val request = rawRequest.withHeaders(headers:_*).withRequestTimeout(requestTimeout.duration.toMillis)
-    
+
       RequestHandler[Either[Result.Error[Nothing], Result.Success[Info]]](request, "GET", identity[play.api.libs.ws.WSRequest]) {
         case resp if resp.status == 200 =>
           resp.json.validate[Info].fold(
@@ -113,7 +129,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
           Left(Result.Error(resp.status, message = Some("Unexpected response code"), rawResponse = resp))
       }
     }
-           
+
     def putRaw(rawRequest: WSRequest, body: Info, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]] = {
       require(Seq(ContentTypes.ApplicationJson) contains contentType, "Unacceptable contentType specified")
       val headers = Seq(
@@ -122,7 +138,7 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
         xFlowId.map("X-Flow-Id" -> _.toString)
       ).flatten
       val request = rawRequest.withHeaders(headers:_*).withRequestTimeout(requestTimeout.duration.toMillis)
-    
+
       RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]](request, "PUT", _.withBody(Json.toJson(body))) {
         case resp if resp.status == 200 =>
           Right(Result.Success(resp.status, message = Some("Successful"), rawResponse = resp))
@@ -143,23 +159,91 @@ class ClientImpl(baseUrl: String, wsClient: => WSClient) extends Client {
       
       listRaw(wsClient.url(s"$baseUrl/api/sales-channels/${play.utils.UriEncoding.encodePathSegment(salesChannelId.toString, "UTF-8")}/infos"), xFlowId, acceptEncoding, ifModifiedSince, contentType)
     }
-           
+
     def post(salesChannelId: tags.ids.SalesChannelId, body: Info, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]] = {
-      
+
       postRaw(wsClient.url(s"$baseUrl/api/sales-channels/${play.utils.UriEncoding.encodePathSegment(salesChannelId.toString, "UTF-8")}/infos"), body, xFlowId, contentType)
     }
-           
+
     def get(salesChannelId: tags.ids.SalesChannelId, infoId: tags.ids.InfoId, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Info]]] = {
-      
+
       getRaw(wsClient.url(s"$baseUrl/api/sales-channels/${play.utils.UriEncoding.encodePathSegment(salesChannelId.toString, "UTF-8")}/infos/${play.utils.UriEncoding.encodePathSegment(infoId.toString, "UTF-8")}"), xFlowId, contentType)
     }
-           
+
     def put(salesChannelId: tags.ids.SalesChannelId, infoId: tags.ids.InfoId, body: Info, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[Nothing]]] = {
-      
+
       putRaw(wsClient.url(s"$baseUrl/api/sales-channels/${play.utils.UriEncoding.encodePathSegment(salesChannelId.toString, "UTF-8")}/infos/${play.utils.UriEncoding.encodePathSegment(infoId.toString, "UTF-8")}"), body, xFlowId, contentType)
     }
-           
+
   }
-       
+
+  object InfosClient extends InfosClient {
+    def listRaw(rawRequest: WSRequest, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationInfoPageJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[InfoPage]]] = {
+      require(Seq(ContentTypes.ApplicationInfoPageJson) contains contentType, "Unacceptable contentType specified")
+      val headers = Seq(
+        "Content-Type" -> s"${contentType.value}; charset=UTF-8"
+      ) ++ Seq(
+        xFlowId.map("X-Flow-Id" -> _.toString)
+      ).flatten
+      val request = rawRequest.withHeaders(headers:_*).withRequestTimeout(requestTimeout.duration.toMillis)
+
+      RequestHandler[Either[Result.Error[Nothing], Result.Success[InfoPage]]](request, "GET", identity[play.api.libs.ws.WSRequest]) {
+        case resp if resp.status == 200 =>
+          resp.json.validate[InfoPage].fold(
+            invalid = errors => Left(Result.Error(resp.status, Some("Invalid body for 'InfoPage': " + errors.mkString(", ")), rawResponse = resp)),
+            valid = body => Right(Result.Success(resp.status, message = Some("Successful"), Some(body), rawResponse = resp))
+          )
+        case resp =>
+          Left(Result.Error(resp.status, message = Some("Unexpected response code"), rawResponse = resp))
+      }
+    }
+
+    def list(limit: Option[Int] = None, nextCursor: Option[String] = None, salesChannelId: Option[tags.ids.SalesChannelId] = None, xFlowId: Option[tags.ids.FlowId], contentType: ContentType = ContentTypes.ApplicationInfoPageJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Nothing], Result.Success[InfoPage]]] = {
+      val queryParams = Seq(
+        limit.map("limit" -> _.toString),
+        nextCursor.map("next_cursor" -> _.toString),
+        salesChannelId.map("sales_channel_id" -> _.toString)
+      ).flatten
+      listRaw(wsClient.url(s"$baseUrl/api/infos").withQueryString(queryParams:_*), xFlowId, contentType)
+    }
+
+  }
+
+  object BatchSalesChannelsInfosClient extends BatchSalesChannelsInfosClient {
+    def postRaw(rawRequest: WSRequest, body: BatchInfo, xFlowId: Option[tags.ids.FlowId], xRequestGroupId: Option[tags.ids.RequestGroupId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Seq[Problem]], Result.Success[UpdateInfos]]] = {
+      require(Seq(ContentTypes.ApplicationJson) contains contentType, "Unacceptable contentType specified")
+      val headers = Seq(
+        "Content-Type" -> s"${contentType.value}; charset=UTF-8"
+      ) ++ Seq(
+        xFlowId.map("X-Flow-Id" -> _.toString),
+        xRequestGroupId.map("X-Request-Group-Id" -> _.toString)
+      ).flatten
+      val request = rawRequest.withHeaders(headers:_*).withRequestTimeout(requestTimeout.duration.toMillis)
+
+      RequestHandler[Either[Result.Error[Seq[Problem]], Result.Success[UpdateInfos]]](request, "POST", _.withBody(Json.toJson(body))) {
+        case resp if resp.status == 202 =>
+          resp.json.validate[UpdateInfos].fold(
+            invalid = errors => Left(Result.Error(resp.status, Some("Invalid body for 'UpdateInfos': " + errors.mkString(", ")), rawResponse = resp)),
+            valid = body => Right(Result.Success(resp.status, message = Some("Accepted for asynchronous completion - location URL provided in response header to get further information"), Some(body), rawResponse = resp))
+          )
+        case resp if resp.status == 400 =>
+          Left(resp.json.validate[Seq[Problem]].fold(
+            invalid = errors => Result.Error(resp.status, Some("Invalid body for error body 'Seq[Problem]': " + errors.mkString(", ")), rawResponse = resp),
+            valid = body => Result.Error(resp.status, message = Some("Bad Request"), body = Some(body), rawResponse = resp)
+          ))
+        case resp if resp.status == 429 =>
+          Left(Result.Error(resp.status, message = Some("Too many requests - the caller has issued too many requests, and is being rate limited. "), rawResponse = resp))
+        case resp =>
+          Left(Result.Error(resp.status, message = Some("Unexpected response code"), rawResponse = resp))
+      }
+    }
+
+    def post(salesChannelId: tags.ids.SalesChannelId, body: BatchInfo, xFlowId: Option[tags.ids.FlowId], xRequestGroupId: Option[tags.ids.RequestGroupId], contentType: ContentType = ContentTypes.ApplicationJson)(implicit requestTimeout: RequestTimeout[Client]): RequestHandler[Either[Result.Error[Seq[Problem]], Result.Success[UpdateInfos]]] = {
+
+      postRaw(wsClient.url(s"$baseUrl/api/batch/sales-channels/${play.utils.UriEncoding.encodePathSegment(salesChannelId.toString, "UTF-8")}/infos"), body, xFlowId, xRequestGroupId, contentType)
+    }
+
+  }
+
 }
        
